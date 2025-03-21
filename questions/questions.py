2eb44c8c383eb76c70dc164c65887d8e99e2c21f
ezeweber <ezeweber2003@gmail.com>
2025-03-21 12:44:38 -0300

    Correcion de comentarios

--- questions/questions.py
+++ questions/questions.py
@@ -23,13 +23,12 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
-# 3er Inciso
+# 3er Inciso, seleccion aleatoria (y sin repetir) de una pregunta, sus posibles rtas y la correcta
 preguntas = random.sample(list(zip(questions, answers, correct_answers_index)), k=3)
 # 2do Inciso puntaje
 puntaje = 0
-# El usuario deberá contestar 3 preguntas
+# Itera 3 veces (k) las preguntas previamente seleccionadas en la lista de tuplas (preguntas)
 for pregunta, opciones, respuesta_correcta in preguntas:
-# Imprime una pregunta aleatoria la cantidad de veces de mi variable cant 
     print(pregunta)
 # Se muestra las respuestas posibles
     for i, answer in enumerate(opciones):
@@ -53,16 +52,17 @@ for pregunta, opciones, respuesta_correcta in preguntas:
 # Sino tiene otro intento
                         print("\nIncorrecto. Intente nuevamente\n")
                         puntaje -= 0.5
+# Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
                         if (intento==1):
                             print(f"\nLa respuesta correcta era: {opciones[respuesta_correcta]}")
+# Tiene 2 salidas, (else) si no responde un numero (case _) respondio un numero que no sea del 1 al 4
                 case _:
                     print("Respuesta no válida")
                     sys.exit(1)        
         else:
             print("Respuesta no válida")
             sys.exit(1)
-# Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
 # Se imprime un blanco al final de la pregunta
     print()
-# Final de la partida
+# Mensaje final de la partida
 print(f"El puntaje obtenido por el jugador/a fue de: {puntaje} puntos, sobre un total de 3 puntos posibles")
